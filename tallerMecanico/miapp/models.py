3f95d0d4fddc793eea0ae22723b5d718c9671c28
bastianPino <bast.pino@duocuc.cl>
2024-07-12 19:58:39 -0400

    se crea models
    se crea el archivo models con los modelos corrrespondientes.

--- tallerMecanico/miapp/models.py
+++ tallerMecanico/miapp/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.conf import settings
+
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, default=make_password('default_password'))
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15)
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions')
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    año = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+    stock = models.PositiveIntegerField(default=0)  # Agregar el campo de stock
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart({self.user})"
+
+    def get_total(self):
+        total = sum(item.get_total_price() for item in self.items.all())
+        return total
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"CartItem({self.product}, {self.quantity})"
+
+    def get_total_price(self):
+        return self.product.precio * self.quantity
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order({self.user}, {self.total})"

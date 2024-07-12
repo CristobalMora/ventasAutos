1669ed7a1eb1653c23aef333a261586f28fa7479
CristobalMora <cri.mora@duocuc.cl>
2024-07-12 19:50:49 -0400

    se agrega adjuntas archivos

--- tallerMecanico/tallerMecanico/urls.py
+++ tallerMecanico/tallerMecanico/urls.py
@@ -1,22 +1,34 @@
-"""
-URL configuration for tallerMecanico project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from miapp import views 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login_view'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('producto/', views.producto, name='producto'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('custom_login/', views.CustomLoginView.as_view(), name='custom_login'),
+    path('registrar_producto/', views.registrar_producto, name='registrar_producto'),
+    path('lista_productos/', views.lista_productos, name='lista_productos'),  # Asegúrate de que esta línea esté presente
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('remove_from_cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('product_list/', views.product_list, name='product_list'),
+    path('edit_product/', views.edit_product, name='edit_product'),
+    path('reporte_ventas/', views.reporte_ventas, name='reporte_ventas'),
+    path('descargar-reporte-ventas/', views.descargar_reporte_ventas, name='descargar_reporte_ventas'),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.conf import settings
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

8e279eb2897b00f75e472bbc60b66f53c993e8e4
CristobalMora <cri.mora@duocuc.cl>
2024-07-12 20:03:40 -0400

    se crea admin

--- tallerMecanico/miapp/admin.py
+++ tallerMecanico/miapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Producto
 
 # Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email')  # Campos a mostrar en la lista de clientes
+    search_fields = ('nombre', 'email')
+
+# admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Producto)
